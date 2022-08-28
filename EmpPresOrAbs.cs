1c43a1be149e3f45eead642052d6781ddab3d44a
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-08-28 13:18:51 +0530

    Present abscent check

---
+++ EmpPresOrAbs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWage
+{
+    internal class EmpPresOrAbs
+    {
+        public static void UC1()
+        {
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);// 0 1
+
+            switch (empcheck)
+            {
+                case 0:
+                    Console.WriteLine("is present");
+                    break;
+                case 1:
+                    Console.WriteLine("is abscent");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
